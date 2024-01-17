220567867e832a994fcbe71275cd790413b31e74
denimozh <denimozh@gmail.com>
2024-01-17 19:56:33 +0000

    Completed implemeting API request to pull products from the database in the correct pages

---
+++ prisma/migrations/20240117185111_first/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `options` on the `Product` table. All the data in the column will be lost.
+  - Added the required column `calories` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `carbs` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `fat` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `protiens` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `weight` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "options",
+ADD COLUMN     "addOns" JSONB[],
+ADD COLUMN     "calories" DECIMAL(65,30) NOT NULL,
+ADD COLUMN     "carbs" DECIMAL(65,30) NOT NULL,
+ADD COLUMN     "fat" DECIMAL(65,30) NOT NULL,
+ADD COLUMN     "protiens" DECIMAL(65,30) NOT NULL,
+ADD COLUMN     "weight" DECIMAL(65,30) NOT NULL;
