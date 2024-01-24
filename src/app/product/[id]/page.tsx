cce5b76046a789cf922c07fa86fa3faa5d7ee78e
denimozh <denimozh@gmail.com>
2024-01-24 21:13:17 +0000

    Added add product to cart functionality using Zustand

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,5 @@
 
-import React, {useState} from 'react'
+import React from 'react'
 import {  singleProduct } from '@/constants'
 import Button from '@/components/Button'
 import { Checkbox } from '@mui/material';
@@ -21,7 +21,7 @@ const SingleProduct = async ({params}:{params:{id:string}}) => {
 
   const singleProduct : ProductType = await getData(params.id)
 
-  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
 
   return (
     <div className='flex flex-row gap-10 xl:px-40 py-16'>
@@ -52,27 +52,7 @@ const SingleProduct = async ({params}:{params:{id:string}}) => {
         <p className='text-5xl text-red-600 font-medium underline decoration-4 underline-offset-[28px] pb-16'>{singleProduct.title}</p>
         <p className='text-xl text-red-700'>{singleProduct.description}</p>
         <p className='py-8 text-2xl text-red-700'>Approx Weight: {singleProduct.weight}g</p>
-        <div className='flex flex-row items-center gap-16 pb-16'>
-          <Button text='ADD TO CART' containerStyles='w-fit border-l-2 border-b-4 hover:bg-red-500 hover:border-red-800 active:bg-red-700 border-red-700 bg-red-500 py-5 px-7 rounded-full' textStyles='pl-4 pr-4 text-white font-bold' 
-                  rightIcon='/menu.svg' iconStyle='p-2 rounded-lg w-8 h-8 bg-red-600' imgStyle='p-1'/>
-          <p className='text-3xl text-red-600 font-bold'>${singleProduct.price}</p>
-        </div>
-        <div className='border-2 border-red-600'>
-          <div className='flex flex-row'>
-            <div className='basis-1/3 p-3 bg-red-500 font-bold'>
-              <p className='text-xl text-white'>Additive</p>
-            </div>
-            <div className='basis-1/3 p-3 text-red-400 font-bold border-r-2 border-b-2 border-red-600'>
-              <p className='text-xl'>Beverages</p>
-            </div>
-            <div className='basis-1/3 p-3 text-red-400 font-bold border-r-2 border-b-2 border-red-600'>
-              <p className='text-xl'>Mochi</p>
-            </div>
-          </div>
-          <div className=''>
-            <Price product={singleProduct} price={singleProduct.price}/>
-          </div>
-        </div>
+        <Price product={singleProduct}/>
       </div>
     </div>
   )
